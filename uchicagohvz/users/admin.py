d3f1ad2418788e5419378c004108a05dce481d98
kz26 <whitehat2k9@gmail.com>
2013-10-02 11:15:26 -0500

    Make Profile searchable in admin

--- uchicagohvz/users/admin.py
+++ uchicagohvz/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from uchicagohvz.users.models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	search_fields = ('user__username', 'user__first_name', 'user__last_name')
+
+admin.site.register(Profile, ProfileAdmin)
 
 # Register your models here.
