8d70031b5ab5e634d32ce62a1d63e6d0a8647c9d
kz26 <whitehat2k9@gmail.com>
2013-10-02 05:01:46 -0500

    Add About and Contact pages

--- uchicagohvz/urls.py
+++ uchicagohvz/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,4 +14,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('uchicagohvz.game.urls')),
     url(r'^users/', include('uchicagohvz.users.urls')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
