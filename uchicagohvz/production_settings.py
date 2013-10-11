20ed56d04f029fa4121b23db94dda19167fd054e
kz26 <whitehat2k9@gmail.com>
2013-10-11 10:24:56 -0500

    Change over to local email server in production

--- uchicagohvz/production_settings.py
+++ uchicagohvz/production_settings.py
@@ -30,7 +30,8 @@ REST_FRAMEWORK = {
 
 # Mandrill email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mandrillapp.com'
-from secrets import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
+#from secrets import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+#EMAIL_HOST = 'smtp.mandrillapp.com'
+#EMAIL_PORT = '587'
+EMAIL_HOST = 'localhost'
+#EMAIL_USE_TLS = True
