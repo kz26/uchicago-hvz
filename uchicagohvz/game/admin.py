d3f95125680760fad0efded97713ffc2772e39c7
kz26 <whitehat2k9@gmail.com>
2014-02-17 20:53:12 -0600

    Remove __name from admin

--- uchicagohvz/game/admin.py
+++ uchicagohvz/game/admin.py
@@ -8,7 +8,7 @@ from uchicagohvz.game.models import *
 admin.site.register(Game)
 
 class PlayerAdmin(admin.ModelAdmin):
-	list_filter = ('game__name', 'active', 'human', 'dorm', 'renting_gun', 'gun_requested', 'gun_returned', 'major')
+	list_filter = ('game', 'active', 'human', 'dorm', 'renting_gun', 'gun_requested', 'gun_returned', 'major')
 	if not settings.DEBUG:
 		readonly_fields = ('major',)
 	search_fields = ('user__username', 'user__first_name', 'user__last_name', 'bite_code')
@@ -35,7 +35,7 @@ class PlayerAdmin(admin.ModelAdmin):
 	players_to_csv.short_description = 'Export to CSV'
 
 class KillAdmin(admin.ModelAdmin):
-	list_filter = ('killer__game__name',)
+	list_filter = ('killer__game',)
 	readonly_fields = ('parent',)
 	search_fields = ('killer__user__username', 'killer__user__first_name', 'killer__user__last_name', 
 		'victim__user__username', 'victim__user__first_name', 'victim__user__last_name')
