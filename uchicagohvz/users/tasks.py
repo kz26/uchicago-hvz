7530939ef54455dc70b0ea037fbece3af3255ef2
kz26 <whitehat2k9@gmail.com>
2014-02-24 01:36:40 -0600

    Rate limit Sympa task

--- uchicagohvz/users/tasks.py
+++ uchicagohvz/users/tasks.py
@@ -2,7 +2,7 @@ from celery import task
 from django.conf import settings
 from django.core import mail
 
-@task
+@task(rate_limit=0.2)
 def do_sympa_update(user, listname, subscribe):
 	if subscribe:
 		body = "QUIET ADD %s %s %s" % (listname, user.email, user.get_full_name())
