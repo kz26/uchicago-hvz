1881abf0fab6099fbba287d6b53215561c013d70
kz26 <whitehat2k9@gmail.com>
2014-02-25 19:03:34 -0600

    Rename pk to id

--- uchicagohvz/game/serializers.py
+++ uchicagohvz/game/serializers.py
@@ -4,7 +4,7 @@ from uchicagohvz.game.models import *
 class KillSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Kill
-		fields = ('pk', 'killer', 'victim', 'location', 'date', 'points')
+		fields = ('id', 'killer', 'victim', 'location', 'date', 'points')
 
 	killer = serializers.SerializerMethodField('get_killer')
 	victim = serializers.SerializerMethodField('get_victim')
