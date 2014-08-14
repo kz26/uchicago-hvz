936706cde18f31c6ab9393555485a71d0441efcd
Chris <chris.eidsmoe@gmail.com>
2014-08-14 16:26:28 -0500

    Profile page, rough user registration, aesthetic/user friendliness changes to analytics

--- uchicagohvz/urls.py
+++ uchicagohvz/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
--- uchicagohvz/users/views.py
+++ uchicagohvz/users/views.py
@@ -5,6 +5,10 @@ from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.views.generic import *
 from uchicagohvz.users.forms import *
+from uchicagohvz.users.models import *
+from uchicagohvz.game.models import *
+from uchicagohvz.game.templatetags.game_extras import pp_timedelta
+import datetime
 
 # Create your views here.
 
@@ -14,9 +18,60 @@ def login(request):
 def logout(request):
 	return auth_views.logout(request, "/")
 
+class RegisterUser(FormView):
+	form_class = UserRegistrationForm
+	template_name = "users/register.html"
+
+	def form_valid(self, form):
+		user = form.save(commit=False)
+		user.set_password(user.password)
+		user.save()
+		messages.success(self.request, "You can now register for games!")
+		return HttpResponseRedirect('/')
+
+	def get_context_data(self, **kwargs):
+		context = super(RegisterUser, self).get_context_data(**kwargs)
+		return context
+
+class ShowProfile(DetailView):
+	model = Profile
+	template_name = "users/profile.html"
+
+	def get_context_data(self, **kwargs):
+		def add(x,y): return x+y
+		context = super(ShowProfile, self).get_context_data(**kwargs)
+		if self.request.user.is_authenticated():
+			player_list = Player.objects.filter(user=self.request.user)
+			total_kills = 0
+			total_human_points = 0
+			total_zombie_points = 0
+			deaths = 0
+			lifespans = []
+			if player_list:
+				for player in player_list:
+					total_kills += len(player.kills)
+					total_zombie_points += player.zombie_points
+					total_human_points += player.human_points
+					lifespans.append(player.lifespan)
+					if player.human == False:
+						deaths += 1
+			
+				context['deaths'] = deaths
+				context['total_kills'] = total_kills
+				context['total_zombie_points'] = total_zombie_points
+				context['total_human_points'] = total_human_points
+				if len(lifespans) > 0:
+					context['average_lifespan'] = pp_timedelta(reduce(add, lifespans) / len(lifespans))
+				else:
+					context['average_lifespan'] = 0
+				context['longest_life'] = pp_timedelta(max(lifespans))
+				context['participation'] = len(player_list)
+
+		return context
+
 class UpdateProfile(UpdateView):
 	form_class = ProfileForm
-	template_name = "users/profile.html"
+	template_name = "users/update_profile.html"
 
 	@method_decorator(login_required)
 	def dispatch(self, request, *args, **kwargs):
