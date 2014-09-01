e10145c369c187f546112ca32bde6739ad8412f9
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-01 00:47:14 -0400

    width of zed squad table fixed, added color field to game model

--- uchicagohvz/game/models.py
+++ uchicagohvz/game/models.py
@@ -31,7 +31,8 @@ class Game(models.Model):
 	start_date = models.DateTimeField()
 	end_date = models.DateTimeField()
 	rules = models.FileField(upload_to=gen_rules_filename, storage=OverwriteFileSystemStorage())
-	
+	color = models.CharField(max_length=64)	
+
 	objects = GameManager()
 
 	def __unicode__(self):
