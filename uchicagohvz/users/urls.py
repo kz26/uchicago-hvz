a9b8d3436d168b564c89c2d01d63ad4e3d61c706
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-10-03 20:54:26 -0500

    added a slash to make the stats page url less ugly

--- uchicagohvz/users/urls.py
+++ uchicagohvz/users/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^login/$', login, name="users|login"),
     url(r'^logout/$', logout, name="users|logout"),
-    url(r'^profile(?P<pk>[0-9]+)/$', ShowProfile.as_view(), name="users|profile"),
+    url(r'^profile/(?P<pk>[0-9]+)/$', ShowProfile.as_view(), name="users|profile"),
     url(r'^update_profile/$', UpdateProfile.as_view(), name="users|update_profile"),
     url(r'^register/$', RegisterUser.as_view(), name="users|register"),
 
