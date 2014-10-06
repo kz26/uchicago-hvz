fd176e39be7fc655319a44b9a6dc0a7b33bba284
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-10-06 13:14:45 -0500

    "your squad" only shows up if there are squads

--- uchicagohvz/game/views.py
+++ uchicagohvz/game/views.py
@@ -48,6 +48,7 @@ class ShowGame(DetailView):
 				context['most_infectious_dorms'] = most_infectious_dorms(self.object)
 				context['top_humans'] = top_humans(self.object)[:10]
 				context['top_zombies'] = top_zombies(self.object)[:10]
+				context['squad_count'] = self.object.squads.count()
 				if self.object.squads.count():
 					context['top_human_squads'] = top_human_squads(self.object)
 					context['top_zombie_squads'] = top_zombie_squads(self.object)
