ad30fea021ca15d66d5fec680bfb59421f38004b
Kevin Zhang <whitehat2k9@gmail.com>
2015-02-25 22:31:33 -0500

    Middleware improvements

--- uchicagohvz/game/middleware.py
+++ uchicagohvz/game/middleware.py
@@ -1,20 +1,23 @@
-from django.core.urlresolvers import resolve, reverse, Resolver404
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils import timezone
+from django.contrib import messages
 from datetime import datetime
 
 class Feb262015Middleware(object):
-	target_url = 'feb-26-2015-charlie-hebdo'
+
+	msg_content = """
+	<p>IMPORTANT: Gameplay will be suspended February 26th from 6am-11:59pm.
+	<a class="alert-link" href="/feb-26-2015/">Read the bulletin</a> for more details.</p>
+	"""
 	
 	def process_request(self, request):
-		try:
-			rm = resolve(request.path)
-			if rm.url_name == self.target_url:
-				return None
-		except Resolver404:
-			pass
-		start_dt = datetime(2015, 2, 26, 6, tzinfo=timezone.get_default_timezone())
-		end_dt = datetime(2015, 2, 26, 23, 59, 59, tzinfo=timezone.get_default_timezone())
-		if start_dt <= timezone.now() <= end_dt:
-			return HttpResponseRedirect(reverse(self.target_url))
-		return None
+		if request.path != '/feb-26-2015/':
+			start_dt = datetime(2015, 2, 26, 6, tzinfo=timezone.get_default_timezone())
+			end_dt = datetime(2015, 2, 26, 23, 59, 59, tzinfo=timezone.get_default_timezone())
+			if start_dt <= timezone.now() <= end_dt:
+				return HttpResponseRedirect('/feb-26-2015/')
+			elif timezone.now() < start_dt:
+				messages.error(request, self.msg_content)
+		else:
+			return None
