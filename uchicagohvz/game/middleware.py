3ffbd3326a9fa4da075ac3ec9370f6ea395069aa
Kevin Zhang <whitehat2k9@gmail.com>
2015-02-25 22:32:46 -0500

    mark_safe fix

--- uchicagohvz/game/middleware.py
+++ uchicagohvz/game/middleware.py
@@ -1,15 +1,16 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.contrib import messages
 from datetime import datetime
 
 class Feb262015Middleware(object):
 
-	msg_content = """
+	msg_content = mark_safe("""
 	<p>IMPORTANT: Gameplay will be suspended February 26th from 6am-11:59pm.
 	<a class="alert-link" href="/feb-26-2015/">Read the bulletin</a> for more details.</p>
-	"""
+	""")
 	
 	def process_request(self, request):
 		if request.path != '/feb-26-2015/':
