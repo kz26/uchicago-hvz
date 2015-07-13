24c2c94ebc3b4118902272a1d4fc34354134add0
Greg Linklater <greglink49@gmail.com>
2015-07-13 13:30:41 +0200

    fixing static_root config
    Signed-off-by: Greg Linklater <greglink49@gmail.com>

--- uchicagohvz/local_settings.py
+++ uchicagohvz/local_settings.py
@@ -138,6 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django Compressor
 STATICFILES_FINDERS = (
