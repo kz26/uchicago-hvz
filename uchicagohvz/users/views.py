1014b9abebeff2bb2cafa9b419228158add23932
Agiel882 <agiel882@gmail.com>
2019-02-27 22:11:40 +0000

    Add discord tag back to user account form

--- uchicagohvz/users/views.py
+++ uchicagohvz/users/views.py
@@ -130,6 +130,15 @@ class MyAccount(UpdateView):
 		return get_object_or_404(Profile, user=self.request.user)
 
 	def form_valid(self, form):
+		user = self.request.user
+		current_game = Game.objects.all()[0];
+		try:
+			current_player = current_game.players.get(user_id = user.id)
+		except Player.DoesNotExist:
+			current_player = None
+		tag = form.clean().get('discord_tag')
+		if(tag != "" and current_player):
+			webhook_send_command("!register_player %s %d" %(tag, current_player.human))
 		messages.success(self.request, "Account settings updated successfully.")
 		return super(MyAccount, self).form_valid(form)
 
